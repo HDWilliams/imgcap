64bca5fb9f69bfff528323b6684c757e65ebb947
hugh <hughdaywilliams@gmail.com>
2023-10-24 22:44:52 -0700

    saving updated templates

--- views.py
+++ views.py
@@ -1,11 +1,12 @@
 from app import app
-from flask import redirect, url_for, render_template, request
+from flask import send_file, redirect, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from models.Img import Img
 import os
 from utilities.DbStorage import DbStorage
 from db import db
+import io
 
 import base64
 
@@ -16,33 +17,25 @@ def home():
 @app.route('/upload', methods = ['POST'])
 #handle img uploading and save to db
 def upload():
-    print(request.files)
-    if request.method == 'POST':
-        #check if there is a file
-        file = request.files['file']
-        if 'file' not in request.files:
-            return 'No file found', 400
-        file = request.files['file']
-        if file.filename == '':
-            return 'No selected file', 400
-        filename = secure_filename(file.filename)
-        file_path = os.path.join('/temp', filename)
-        file.save(file_path)
-
-
-        with open(file_path, 'rb') as f:
-            DbStore = DbStorage()
-            img:Img = Img(name=filename, img=DbStore.convert_b64(f.read()))
-            db.session.add(img)
-            db.session.commit()
+    file = request.files['image']
+    #DbStore = DbStorage()
+    img:Img = Img(name=file.filename, img=file.read())
+    print(len(file.read()))
+    db.session.add(img)
+    db.session.commit()
+        
     return redirect(url_for('display'))
 
 @app.route('/display')
 def display():
     file: Img = Img.query.first()
-    decoded_image = base64.b64decode(file.img)
-    image_url = "data:image/png;base64," + base64.b64encode(decoded_image).decode('utf-8')
-    return render_template('display.html', image_url=image_url)
+    
+    image_data = base64.b64encode(file.img).decode('ascii')
+    if image_data is not None:
+        return render_template('display.html', image_data = image_data)
+    else:
+        return 'Image Not Found', 404
+
 
 
 
