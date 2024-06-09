a7d731b5b7741000e48cda3a15fbf1508f645edf
HDWilliams <hughdaywilliams@gmail.com>
2024-06-09 08:54:06 -0700

    added overlay to image delete. fixed delete button form interference

--- views.py
+++ views.py
@@ -1,13 +1,14 @@
 """Handles all app routing including 
 displaying images, uploading images, search and delete"""
-from flask import redirect, url_for, render_template, jsonify, request
+from flask import redirect, url_for, render_template, jsonify,flash,request
 from flask_sqlalchemy import SQLAlchemy
+from psycopg2 import OperationalError
+import logging
 from app import app
 from models.All import Img, Tag, ImgTags
 from db import db
 from app import fasttext_model
 import utilities.DbInterface as DbInterface
-
 from utilities.ImageProcessing import process_image, delete_image_and_metadata
 from helpers.prep_images_for_display import prep_images_for_display
 
@@ -15,7 +16,17 @@ from helpers.prep_images_for_display import prep_images_for_display
 def home():
     """SPLITS IMAGES INTO TWO GROUPS FOR DISPLAY ON TWO COLUMNS ON HOMEPAGE
     TO ADD ORDERING BY DATE ADDED"""
-    images = Img.query.all()
+    images = []
+    try:
+        images = Img.query.all()
+    except OperationalError as e:
+        logging.exception(e)
+        raise ConnectionError('Unable to connect to server. Please check connection')
+    except Exception as e:
+        logging.exception(e)
+        flash('Could not load images. Please check connection and try again')
+        raise ConnectionError('Unable to connect to server. This may be a server issue, please check connection and try again')
+
     return render_template('index.html', images=prep_images_for_display(images))
 
 @app.route('/upload', methods = ['POST'])
@@ -27,16 +38,23 @@ def upload():
     """
 
     file = request.files['file']
-    process_image(file)
+    try:
+        process_image(file)
+    except Exception as e:
+        logging.exception(e)
+        flash('Image could not be processed. Please try again')
     return redirect(url_for('home'))
 
 @app.route('/autocomplete')
 def autocomplete():
     """RETURNS ALL AVAILABLE TAGS 
     FOR SEARCH BAR AUTOCOMPLETE"""
-
-    tags = db.session.query(Tag).all()
-    tags = [tag.value for tag in tags]
+    tags = []
+    try:
+        tags = db.session.query(Tag).all()
+        tags = [tag.value for tag in tags]
+    except Exception as e:
+        logging.exception(e)
     return jsonify(tags)
 
 @app.route('/search', methods = ['GET'])
